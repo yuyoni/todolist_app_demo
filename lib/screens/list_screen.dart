ae1c6d84d75b890184eb1bd3d32820c3c389bb2e
yuyoni <yuyeon116@gmail.com>
2023-09-19 19:39:21 +0900

    9/19

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,11 +1,81 @@
 // lib/screens/list_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_todolist_app/models/todo.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  List<Todo> todos = [];
+  bool isLoading = true;
+
+  //생성자
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('ListScreen')),
-    );
+        appBar: AppBar(title: Text('할 일 목록 앱'), actions: [
+          InkWell(
+              onTap: () {},
+              child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.book),
+                      Text('뉴스'),
+                    ],
+                  )))
+        ]),
+        floatingActionButton: FloatingActionButton(
+          child: Text(
+            '+',
+            style: TextStyle(fontSize: 25),
+          ),
+          onPressed: () {},
+        ),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.separated(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(todos[index].title),
+                    onTap: () {},
+                    trailing: Container(
+                      width: 80,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                              padding: EdgeInsets.all(5),
+                              child: InkWell(
+                                child: Icon(Icons.edit),
+                                onTap: () {},
+                              )),
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            child: InkWell(
+                              child: Icon(Icons.delete),
+                              onTap: () {},
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+              ));
   }
 }
